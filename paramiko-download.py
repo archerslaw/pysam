71ebd78438d56317b95314bc61880aae2bebd6ca
Sibiao Luo <sibiaoluo@gmail.com>
2013-07-31 22:43:06 +0800

    use paramiko to download file from remote host

---
+++ paramiko-download.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+#coding:utf-8
+
+import paramiko
+import os
+import datetime
+
+hostname='192.168.3.121'
+username='root'
+password='redhat'
+port=22
+local_dir='/tmp/local/'
+remote_dir='/tmp/remote/'
+
+if __name__=="__main__":
+    try:
+        t=paramiko.Transport((hostname, port))
+        t.connect(username=username,password=password)
+        sftp=paramiko.SFTPClient.from_transport(t)
+        #files=os.listdir(local_dir)
+        files=os.listdir(remote_dir)
+        for f in files:
+            print ''
+            print '####################################################'
+            print 'Beginning to download file %s.' % datetime.datetime.now()
+            print 'Downloading file:',os.path.join(remote_dir,f)
+            sftp.get(os.path.join(remote_dir,f), os.path.join(local_dir,f))
+            #sftp.put(os.path.join(local_dir,f), os.path.join(remote_dir,f))
+            print 'Upload file success %s.' % datetime.datetime.now()
+            print ''
+    except Exception:
+        print "Error! Please check it."
+        t.close()
